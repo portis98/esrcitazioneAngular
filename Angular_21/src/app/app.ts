0f157b79c1b12d19bd5f875ae6faf55463e79188
portis98 <l.porta@itsolivetti.it>
2026-02-17 15:45:41 +0100

    Fine milestone 1

--- Angular_21/src/app/app.ts
+++ Angular_21/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Angular_21');
+  protected readonly title = signal('Esercitaziopne 1');
 }
