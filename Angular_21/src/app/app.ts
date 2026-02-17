237853d726b81c88978e610ee0da2fd05bd8e4e0
portis98 <l.porta@itsolivetti.it>
2026-02-17 16:15:31 +0100

    aggiunta Todos

--- Angular_21/src/app/app.ts
+++ Angular_21/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Todos } from './todos/todos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Todos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
