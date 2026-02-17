6fdc7325d58c72c0013dc13616e41666ff2d0fcf
portis98 <l.porta@itsolivetti.it>
2026-02-17 16:43:17 +0100

    aggiunta delete

--- Angular_21/src/app/app.ts
+++ Angular_21/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Todos } from './todos/todos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Todos],
+  imports: [Todos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Angular_21/src/app/todos/todos.ts
+++ Angular_21/src/app/todos/todos.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-todos',
-  imports: [],
+  imports: [MatCheckboxModule, NgClass],
   templateUrl: './todos.html',
   styleUrl: './todos.css',
 })
 export class Todos {
-
+  todos: Todo[] = [
+  {
+  id: 1,
+  title: 'Studiare Angular',
+  completed: false,
+  },
+  {
+  id: 2,
+  title: 'Fare la spesa',
+  completed: false,
+  },
+  {
+  id: 3,
+  title: 'Portare fuori il cane',
+  completed: false,
+  }
+  ];
+  toggleCompleted(todo: Todo): void {
+  todo.completed = !todo.completed;
+  }
+  getClass(todo: Todo): string {
+  return todo.completed ? 'completed' : '';
+  }
+  deleteItem(todo: Todo): void {
+  this.todos = this.todos.filter(
+    (objetItem: Todo): boolean => objetItem !== todo
+    );
+  }
 }
